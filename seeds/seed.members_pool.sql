9e60e7a6db1181df13e9c2415d3234de4e3df143
AJ Olson <silentx.alex@gmail.com>
2019-12-16 22:44:51 -0500

    added mock-data

---
+++ seeds/seed.members_pool.sql
@@ -0,0 +1,12 @@
+INSERT INTO members_pool ( pool_id, email, giftee, confirmation)
+VALUES
+    (1, 'silentx.alex@gmail.com', 'imajerk@gmail.com', false),
+    (1, 'meow@gmail.com', 'ilovecats@gmail.com', false),
+    (1,'princesstofu@gmail.com', 'silentx.alex@gmail.com', false),
+    (1,'puns@gmail.com', 'drk@gmail.com', false),
+    (1,'brokenojoke@gmail.com', 'pld@gmail.com', false),
+    (1,'imajerk@gmail.com', 'meow@gmail.com', true),
+    (1,'drk@gmail.com', 'princesstofu@gmail.com', true),
+    (1,'healers@gmail.com', 'puns@gmail.com', true),
+    (1,'pld@gmail.com', 'healers@gmail.com', true),
+    (1,'ilovecats@gmail.com', 'brokenojoke@gmail.com', true)
---
+++ seeds/seed.pool_table.sql
@@ -0,0 +1,3 @@
+INSERT INTO pool_table (pool_name)
+VALUES 
+    ('Elements Massage Secret Santa 2019')
