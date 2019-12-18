a15793e4a57a41f169403425c83a4118b339f941
AJ Olson <silentx.alex@gmail.com>
2019-12-18 13:16:39 -0500

    added ID and Password to users table, updated Mock data

--- seeds/seed.users.sql
+++ seeds/seed.users.sql
@@ -1,12 +1,12 @@
-INSERT INTO users (name, email)
+INSERT INTO users (name, email, password)
 VALUES
-    ('Alex', 'silentx.alex@gmail.com'),
-    ('Maow', 'puns@gmail.com'),
-    ('Nix', 'healers@gmail.com'),
-    ('Tofu', 'princesstofu@gmail.com'),
-    ('Masky', 'pld@gmail.com'),
-    ('Dilly', 'drk@gmail.com'),
-    ('Poochy', 'brokenojoke@gmail.com'),
-    ('Leyna', 'ilovecats@gmail.com'),
-    ('Charleigh', 'meow@gmail.com'),
-    ('Ollie', 'imajerk@gmail.com')
+    ('Alex', 'silentx.alex@gmail.com', 'Password123'),
+    ('Maow', 'puns@gmail.com', 'Password123'),
+    ('Nix', 'healers@gmail.com', 'Password123'),
+    ('Tofu', 'princesstofu@gmail.com', 'Password123'),
+    ('Masky', 'pld@gmail.com', 'Password123'),
+    ('Dilly', 'drk@gmail.com', 'Password123'),
+    ('Poochy', 'brokenojoke@gmail.com', 'Password123'),
+    ('Leyna', 'ilovecats@gmail.com', 'Password123'),
+    ('Charleigh', 'meow@gmail.com', 'Password123'),
+    ('Ollie', 'imajerk@gmail.com', 'Password123')
