9877cc7b920cc39e61566ac215e04d46a3959a7c
AJ Olson <silentx.alex@gmail.com>
2020-02-03 17:27:56 -0500

    updated cors

--- src/app.js
+++ src/app.js
@@ -20,9 +20,7 @@ const morganOption = (NODE_ENV === 'production')
 
 app.use(morgan(morganOption));
 app.use(helmet());
-app.use(cors({
-    origin: CLIENT_ORIGIN
-}));
+app.use(cors());
 
 app.use('/api/users', usersRouter); // locates user profile
 app.use('/api/pools', poolsRouter); //post req for creating pool
