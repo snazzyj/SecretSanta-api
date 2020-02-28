125d733128d6d33c68bde41cd334da701fa04593
AJ Olson <silentx.alex@gmail.com>
2020-02-27 19:14:23 -0500

    updated db url

--- src/server.js
+++ src/server.js
@@ -1,11 +1,11 @@
 const knex = require('knex');
 const app = require('./app');
 const {PORT} = require('./config');
-const TEST_DATABASE_URL = 'postgresql://Alex:1@localhost/secret-santa-test'
+const DATABASE_URL = 'postgresql://Alex:1@localhost/secret-santa-test'
 
 const db = knex({
   client: 'pg',
-  connection: TEST_DATABASE_URL
+  connection: DATABASE_URL
 })
 
 app.set('db', db)
