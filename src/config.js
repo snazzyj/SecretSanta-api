343fd93c4f64b2b413938be749d91d42504747dd
AJ Olson <silentx.alex@gmail.com>
2020-02-28 17:21:50 -0500

    updated jwt

--- src/config.js
+++ src/config.js
@@ -3,6 +3,6 @@ module.exports = {
     NODE_ENV: process.env.NODE_ENV || 'development',
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://Alex:1@localhost/secret-santa-test',
     DATABASE_URL: process.env.DATABASE_URL || 'postgresql://Alex:1@localhost/secret-santa',
-    JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
+    JWT_SECRET: process.env.JWT_SECRET || 'my-own-special-jwt-secret',
     EMAIL_PASS: 'thinkfulsanta123',
   }
