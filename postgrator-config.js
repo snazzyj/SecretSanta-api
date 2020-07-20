02c92b6ed70db109b1557e363d12eb1ef8373839
AJ Olson <silentx.alex@gmail.com>
2020-07-20 14:58:20 -0400

    updated npm packages

--- postgrator-config.js
+++ postgrator-config.js
@@ -3,8 +3,5 @@ require('dotenv').config();
 module.exports = {
   "migrationDirectory": "migrations",
   "driver": "pg",
-  "connectionString": (process.env.NODE_ENV === 'test')
-  ? process.env.TEST_DATABASE_URL
-  : process.env.DATABASE_URL,
-  "ssl" : !!process.env.SSL
+  "connectionString": process.env.TEST_DATABASE_URL
 }
